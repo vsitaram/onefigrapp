afe1de77ece04c1d8b87d43458f409c3b640067b
Ram Vuppaladadiyam <vsitaram@gmail.com>
2020-04-06 13:10:57 -0400

    Experiment with new views for timeout

--- app/views_2.py
+++ app/views_2.py
@@ -5,7 +5,7 @@ import datetime as datetime
 from dateutil.relativedelta import relativedelta
 
 import json
-from .onefigr_analysis import *
+from .onefigr_analysis import Data
 
 def login(request):
 	template_name = 'app/login.html'
@@ -22,27 +22,30 @@ def tools(request):
 
 @login_required
 def journalsByDiscipline(request):
+    data = Data()
     template_name = 'app/journalsByDiscipline.html'
     context = {
-        'disciplines_list': json.dumps(get_disciplines_list()),
-        'chart_data': json.dumps(journals_by_discipline()),
+        'disciplines_list': json.dumps(data.get_disciplines_list()),
+        'chart_data': json.dumps(data.journals_by_discipline()),
     }
     return render(request, template_name, context)
 
 @login_required
 def journalsByProvider(request):
+    data = Data()
     template_name = 'app/journalsByProvider.html'
     context = {
-        'providers_list': json.dumps(get_provider_list()),
-        'chart_data': json.dumps(journals_by_provider()),
+        'providers_list': json.dumps(data.get_provider_list()),
+        'chart_data': json.dumps(data.journals_by_provider()),
     }
     return render(request, template_name, context)
 
 @login_required
 def providersByMetric(request):
+    data = Data()
     template_name = 'app/providersByMetric.html'
     context = {
-        'providers_list': json.dumps(get_provider_list()),
-        'chart_data': json.dumps(providers_by_metric()),
+        'providers_list': json.dumps(data.get_provider_list()),
+        'chart_data': json.dumps(data.providers_by_metric()),
     }
     return render(request, template_name, context)
