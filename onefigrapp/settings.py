0d35871057c7e8ce070c54dc5de872cb751baca3
Ram Vuppaladadiyam <vsitaram@gmail.com>
2020-05-02 13:56:17 -0400

    enable CORS requests

--- onefigrapp/settings.py
+++ onefigrapp/settings.py
@@ -62,12 +62,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8000',
-    'https://onefigrapp.herokuapp.com'
-)
+# CORS_ORIGIN_WHITELIST = (
+#     'http://localhost:8000',
+#     'https://onefigrapp.herokuapp.com'
+# )
 
 ROOT_URLCONF = 'onefigrapp.urls'
 
