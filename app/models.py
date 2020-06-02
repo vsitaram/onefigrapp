7f75a9c26091e17c6a99cddee6699bfc79ecdae9
Ram Vuppaladadiyam <vsitaram@gmail.com>
2020-06-02 14:05:24 -0600

    Remove models and unnecessary templates

--- app/models.py
+++ app/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+"""
 class File(models.Model):
     upload = models.FileField(default=None)
     uploaded_at = models.DateTimeField(auto_now_add=True, blank=True)
 
     def __str__(self):
         return self.upload.name
+"""
 
 
     
